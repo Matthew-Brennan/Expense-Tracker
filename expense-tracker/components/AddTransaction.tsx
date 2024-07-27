518ce940b198ed80eb7965aebc2906d6aca519fe
Matthew-Brennan <matthew.brennan4@hotmail.com>
2024-07-27 10:12:23 -0400

    clear form

--- expense-tracker/components/AddTransaction.tsx
+++ expense-tracker/components/AddTransaction.tsx
@@ -1,8 +1,10 @@
 "use client";
+import {useRef} from 'react';
 import addTransaction from "@/app/actions/addTransaction";
 import {toast} from 'react-toastify';
 
 const AddTransaction = () => {
+    const fromRef = useRef<HTMLFormElement>(null);
     const clientAction = async (formData: FormData) => {
         const {data, error} = await addTransaction(formData);
 
@@ -10,13 +12,14 @@ const AddTransaction = () => {
             toast.error(error);
         }else{
             toast.success('Transaction Added');
+            fromRef.current?.reset();
         }
     };
 
     return (
     <>
     <h3>Add transaction</h3>
-    <form action={clientAction}>
+    <form ref={fromRef} action={clientAction}>
         <div className="form-contorl">
             <label htmlFor="text">Text</label>
             <input type="text" id="text" name="text" placeholder="Enter Text..."/>

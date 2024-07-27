8e3797ceb28003f2b834a819eb511c708c53f2ff
Matthew-Brennan <matthew.brennan4@hotmail.com>
2024-07-27 12:02:05 -0400

    delete unwanted transactions and fixed the decimal places to 2

--- expense-tracker/components/TransactionItem.tsx
+++ expense-tracker/components/TransactionItem.tsx
@@ -1,13 +1,33 @@
+'use client'
 import { Transaction } from "@/types/transactions";
 import {addCommas} from "@/lib/utlis";
 import {toast} from "react-toastify";
+import deleteTransaction from '@/app/actions/deleteTransaction'
 
 const TransactionItem = ({transaction}: {transaction: Transaction }) => {
+    const sign = transaction.amount < 0 ? '-' : '+'
+
+    const handleDeleteTransaction = async (transactionId: string) => {
+        const confirmed = window.confirm('Are you sure you wish to delete this transaction');
+
+        if(!confirmed) return;
+
+        const {message, error} = await deleteTransaction(transactionId)
+
+        if (error){
+            toast.error(error);
+        }
+
+        toast.success(message);
+    }
+
+
     return (<li className={transaction.amount < 0 ? 'minus' : 'plus'}>
         {transaction.text}
         <span>
             {addCommas(Math.abs(transaction.amount))}
         </span>
+        <button onClick={ () => handleDeleteTransaction(transaction.id)} className="delete-btn">x</button>
     </li>)
 } 
 
